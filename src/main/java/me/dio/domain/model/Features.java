8bce987265153014c405c10bfd7614767d3e2baa
Sthefanie <sthefaniegc06@gmail.com>
2023-10-22 09:51:45 -0300

    Project created

---
+++ src/main/java/me/dio/domain/model/Features.java
@@ -0,0 +1,7 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_features")
+public class Features extends BaseItem {
+}
---
+++ src/test/java/me/dio/projectjavaapi/ProjectJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.projectjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
